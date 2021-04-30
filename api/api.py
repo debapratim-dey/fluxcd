85bb6647917a5324cdac0f0968f6d0e6b11b56f9
Debapratim Dey <debapratim.dey@talentica.com>
2021-04-30 12:10:34 +0530

    testing demo

--- api/api.py
+++ api/api.py
@@ -6,6 +6,6 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>My first flux-cd trial</h1><p>This is an updated image.</p>"
+    return "<h1>Test NS release</h1><p>This is an updated image.</p>"
 
 app.run()
